be22e80a3e970128771951edce7848afecf1ee98
Nurik <N-TNT@MAIL.RU>
2023-03-06 17:05:26 +0600

    Initial Commit

---
+++ homeWork 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWork 1
+//
+//  Created by Нурлан on 6/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
