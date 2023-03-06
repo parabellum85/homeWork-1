4dc4c5fdac40d113ecceb3e3094fff3ac286038e
Nurik <N-TNT@MAIL.RU>
2023-03-06 19:19:04 +0600

    homwWork1

--- homeWork 1/ViewController.swift
+++ homeWork 1/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var LoginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+   
+    
+    @IBAction func buttonAction(_ sender: Any) {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonAction2(_ sender: Any) {
+    }
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if LoginTF.text?.isEmpty ?? true{
+            LoginTF.layer.borderWidth = 2
+            LoginTF.layer.borderColor = UIColor.red.cgColor
+        }
+        if passwordTF.text?.isEmpty ?? true{
+            passwordTF.layer.borderWidth = 2
+            passwordTF.layer.borderColor = UIColor.red.cgColor
+        }
+    }
 }
 
